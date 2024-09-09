56b06ebce7b82b7bc611f5e5ecfc51e8e7c20e91
sumayaalqadhy <sumaya.alqadhy@gmail.com>
2024-09-09 11:43:04 -0700

    updated variable result to remember and use mutableStateOf

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -13,6 +13,10 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -44,6 +48,9 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+        var result by remember { mutableStateOf(1) }
+
+
         Column(
             modifier = modifier,
             horizontalAlignment = Alignment.CenterHorizontally
@@ -53,7 +60,7 @@ class MainActivity : ComponentActivity() {
                 contentDescription = "1"
             )
             Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = { /*TODO*/ }) {
+            Button(onClick = { result = (1..6).random() }) {
                 Text(text = stringResource(R.string.roll))
                 
             }
