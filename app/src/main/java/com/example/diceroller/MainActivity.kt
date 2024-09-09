f94bc127ef9c06a6b06210ba36ea74a53fa29454
sumayaalqadhy <sumaya.alqadhy@gmail.com>
2024-09-09 12:08:20 -0700

    customized background and color

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,10 +21,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -59,22 +64,34 @@ class MainActivity : ComponentActivity() {
             else -> R.drawable.dice_6
         }
 
-
-        Column(
-            modifier = modifier,
-            horizontalAlignment = Alignment.CenterHorizontally
+        Surface(modifier = modifier,
+            color = Color(0xFFFCE3E6)
         ) {
-            Image(
-                painter = painterResource(imageResource),
-                contentDescription = result.toString()
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = { result = (1..6).random() }) {
-                Text(text = stringResource(R.string.roll))
-                
+
+            Column(
+                modifier = modifier,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(imageResource),
+                    contentDescription = result.toString()
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = { result = (1..6).random() },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFFD6A7D6),
+                        contentColor = Color.White
+                    )
+                ) {
+                    Text(
+                        text = stringResource(R.string.roll),
+                        fontWeight = FontWeight.ExtraBold,
+                        letterSpacing = 4.sp
+                    )
+                }
             }
         }
-
     }
 
     @Preview(showBackground = true)
